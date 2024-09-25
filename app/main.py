277d1077b5cecc111ecec83ce4ee1ae4cc687aee
yanruijie902136 <yanruijie902136@gmail.com>
2024-09-25 18:32:23 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,8 +6,9 @@ import socket
 def main() -> None:
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client_socket, _ = server_socket.accept()
-    client_socket.recv(1024)
-    client_socket.sendall("+PONG\r\n".encode())
+    while True:
+        client_socket.recv(1024)
+        client_socket.sendall("+PONG\r\n".encode())
 
 
 if __name__ == "__main__":
