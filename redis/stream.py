f113188631ead01f3de2c64d7f420c6c496d14e7
yanruijie902136 <yanruijie902136@gmail.com>
2024-09-27 01:55:36 +0800

    codecrafters submit [skip ci]

--- redis/stream.py
+++ redis/stream.py
@@ -1,2 +1,34 @@
+from __future__ import annotations
+
+import dataclasses
+
+
+@dataclasses.dataclass(order=True)
+class StreamEntryId:
+    msec: int
+    seq_number: int
+
+    @classmethod
+    def from_string(cls, string: str) -> StreamEntryId:
+        msec, seq_number = map(int, string.split("-"))
+        return StreamEntryId(msec, seq_number)
+
+    def __str__(self) -> str:
+        return f"{self.msec}-{self.seq_number}"
+
+
+@dataclasses.dataclass
+class StreamEntry:
+    entry_id: StreamEntryId
+    data: dict[str, str]
+
+
 class RedisStream:
-    pass
+    def __init__(self):
+        self._entries: list[StreamEntry] = []
+
+    def add_entry(self, entry_id: StreamEntryId, data: dict[str, str]) -> bool:
+        if self._entries and entry_id <= self._entries[-1].entry_id:
+            return False
+        self._entries.append(StreamEntry(entry_id, data))
+        return True
