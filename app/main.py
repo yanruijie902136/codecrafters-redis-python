933d30434e4fed0bdc992317533d5cb0e40998af
yanruijie902136 <yanruijie902136@gmail.com>
2025-08-26 10:52:47 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,15 +1,12 @@
-import socket  # noqa: F401
+import asyncio
+
+from .server import RedisServer
 
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
-    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # server_socket.accept() # wait for client
+def main() -> None:
+    server = RedisServer()
+    asyncio.run(server.run())
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
---
+++ app/server.py
@@ -0,0 +1,11 @@
+import asyncio
+
+
+class RedisServer:
+    async def run(self) -> None:
+        server = await asyncio.start_server(self._client_connected_cb, 'localhost', 6379, reuse_port=True)
+        async with server:
+            await server.serve_forever()
+
+    async def _client_connected_cb(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+        pass
